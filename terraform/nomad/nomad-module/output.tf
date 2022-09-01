380d643bbccac3118c1a8ce5ea7f438d6b67f186
Great-Stone <hahohh@gmail.com>
2022-09-01 14:44:01 +0900

    delete nomad terraform code

--- terraform/nomad/nomad-module/output.tf
+++
@@ -1,7 +0,0 @@
-output "server_ip" {
-  value = aws_eip.server.public_ip
-}
-
-output "client_ips" {
-  value = aws_eip.client[*].public_ip
-}
--- terraform/nomad/nomad-module/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "myip_cidr" {
-  description = "내 IP CIDR"
-  default     = "0.0.0.0/0"
-}
-
-variable "client_count" {
-  description = "Nomad Client Count"
-  default     = 2
-}
